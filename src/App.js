ec11c0ad6f21889a34ca68683fdd2b1daca2d073
Naiara <naiaracacique@gmail.com>
2023-03-09 14:10:17 -0300

    (Feat)req 32 ao 35

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import DoneRecipes from './pages/DoneRecipes';
 import FavoriteRecipes from './pages/FavoriteRecipes';
 import RecipeInProgress from './pages/RecipeInProgress';
 import Recipes from './pages/Recipes';
+import './css/RecipeDetails.css';
 
 // Conferir complementação do id do component RecipeDetails.
 // Conferir uso das páginas Drinks e Meals.
--- src/pages/RecipeDetails.jsx
+++ src/pages/RecipeDetails.jsx
@@ -1,18 +1,24 @@
 import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { useLocation, useHistory } from 'react-router-dom';
+import clipboardCopy from 'clipboard-copy';
 import { mealAPI, cockTailAPI } from '../helpers/APIsHandle';
+import shareIcon from '../images/shareIcon.svg';
+import { updateFavoritesStorage, addToFavorites } from '../helpers/FavoriteLocalStorage';
+import whiteHeartIcon from '../images/whiteHeartIcon.svg';
+import blackHeartIcon from '../images/blackHeartIcon.svg';
 
 function RecipeDetails({ match }) {
   const { id } = match.params;
   const [recipeData, setRecipeData] = useState({});
   const [recommendations, setRecommendations] = useState([]);
   const history = useHistory();
-
   const location = useLocation();
   const isMeal = location.pathname.includes('meals');
   const isDrink = location.pathname.includes('drinks');
   const number = 6;
+  const type = isMeal ? 'meal' : 'drink';
+  const [isFavorite, setIsFavorite] = useState(false);
 
   useEffect(() => {
     async function fetchRecipeData() {
@@ -21,7 +27,6 @@ function RecipeDetails({ match }) {
       mealAPI(apiEndpoint + id) : await cockTailAPI(apiEndpoint + id);
       setRecipeData(data[isMeal ? 'meals' : 'drinks'][0] || {});
     }
-
     fetchRecipeData();
   }, [id, location.pathname, isMeal]);
   useEffect(() => {
@@ -31,11 +36,9 @@ function RecipeDetails({ match }) {
         : await mealAPI(recomendationEndpoint);
       setRecommendations(data[isMeal ? 'drinks' : 'meals']?.slice(0, number) || []);
     }
-
     fetchRecommendations();
   }, [isMeal]);
-
-  console.log(recommendations);
+  // fim das requisições
   const {
     strAlcoholic,
     strMealThumb,
@@ -45,7 +48,6 @@ function RecipeDetails({ match }) {
     strCategory: category,
     strInstructions: instructions,
     strYoutube: video,
-
   } = recipeData;
 
   const image = isMeal ? strMealThumb : strDrinkThumb;
@@ -56,19 +58,50 @@ function RecipeDetails({ match }) {
     .filter(([key]) => key.includes('Ingredient') && recipeData[key])
     .map(([key, value], index) => ({
       name: value,
-
       measure: recipeData[`strMeasure${index + 1}`],
       key: { key },
     }));
+
   const handleClickSetRecepies = () => {
     const path = isDrink ? `/drinks/${id}/in-progress` : `/meals/${id}/in-progress`;
     history.push(path);
   };
 
+  const handleShareClick = async () => {
+    const link = window.location.href;
+    await clipboardCopy(link);
+    const tagMsg = document.createElement('p');
+    tagMsg.innerText = 'Link copied!';
+    document.body.appendChild(tagMsg);
+  };
+  const handleFavoritesStorage = () => {
+    const actualStorage = JSON.parse(localStorage.getItem('favoriteRecipes'));
+    const paramObj = {
+      type,
+      storage: actualStorage,
+      actualRecipe: recipeData,
+      actualImg: image,
+      actualId: id,
+    };
+
+    if (actualStorage) {
+      return updateFavoritesStorage(paramObj);
+    }
+    return addToFavorites(type, recipeData, image, id);
+  };
+
+  useEffect(() => {
+    const favoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes')) || [];// Caso não haja nada salvo, é retornado um array vazio.
+    const isRecipeFavorite = favoriteRecipes
+      .find((f) => f.id === id) !== undefined;
+    setIsFavorite(isRecipeFavorite);
+  }, [id]);
+  // find para verificar se o objeto com o id especificado está presente no array favoriteRecipes
   return (
     <div>
       {image && title && (
         <img
+          className="imagem"
           data-testid="recipe-photo"
           src={ image }
           alt={ title }
@@ -110,6 +143,41 @@ function RecipeDetails({ match }) {
           allowFullScreen
         />
       )}
+      <button
+        data-testid="share-btn"
+        type="button"
+        className="btn-compartilhar-recipe"
+        onClick={ () => {
+          handleShareClick();
+        } }
+      >
+        <img
+          src={ shareIcon }
+          alt="Share"
+        />
+        Compartilhar
+      </button>
+
+      <button
+        type="button"
+        className="btn-compartilhar-recipe"
+        onClick={ () => {
+          handleFavoritesStorage();
+          setIsFavorite(!isFavorite);
+        } }
+      >
+        {isFavorite ? <img
+          data-testid="favorite-btn"
+          src={ blackHeartIcon }
+          alt="favoritar"
+        /> : <img
+          data-testid="favorite-btn"
+          src={ whiteHeartIcon }
+          alt="favoritar"
+        /> }
+        Favoritar
+      </button>
+
       <h2>Recommendations:</h2>
 
       <div className="container-carrosel">
@@ -124,36 +192,33 @@ function RecipeDetails({ match }) {
               className="imagemCarrosel"
               src={ item.strMealThumb || item.strDrinkThumb }
               alt={ item.strMeal || item.strDrink }
-
             />
             <p
               data-testid={ `${index}-recommendation-title` }
               className="tileCarrossel"
-
             >
               { item.strMeal || item.strDrink }
-
             </p>
           </div>
 
         ))}
       </div>
-      {/* <Link to={ RecipeInprogress }> */}
       <div>
         <button
           data-testid="start-recipe-btn"
           type="button"
           className="btn-start-recipe"
           onClick={ handleClickSetRecepies }
+
         >
+
+          {' '}
           Start Recipe
         </button>
       </div>
-      {/* </Link> */}
     </div>
   );
 }
-
 RecipeDetails.propTypes = {
   match: PropTypes.shape({
     params: PropTypes.shape({
